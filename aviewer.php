b5d4f1ddb60393eec131a02c05e3bf211e416f29
Joseph Parsons <josephtparsons@gmail.com>
2012-04-29 19:03:52 -0500

    Support configurations between domains.

---
+++ aviewer.php
@@ -0,0 +1,167 @@
+<?php
+/*
+   Copyright 2011 Joseph T. Parsons
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/* I did this without the manual; sue me for using regex. */
+/* A basic overview of this file; note that it is still in early alpha and what-not:
+1. We define all functions.
+2. We check to see if a URL for lookup is specified.
+ 2a. If not, we list all stored URLs.
+ 2b. If it is, we:
+  2bi. Check to see if it is defined in the cache. If it is, we get it and display it.
+  2bii. If not, we determine whether it stored compressed. If so, we uncompress it, otherwise we link it. */
+
+require('aviewerConfiguration.php');
+require('aviewerFunctions.php');
+
+error_reporting(E_ALL);
+$data = '';
+
+$url = isset($_GET['url']) ? (string) $_GET['url'] : false; // Get the URL to display from GET.
+$fileType = isset($_GET['type']) ? (string) $_GET['type'] : false; // Get the URL to display from GET.
+$me = $_SERVER['PHP_SELF']; // This file.
+
+if ($url === false) { // No URL specified.
+  $fileScan = scandir($store); // Read the directory and return each file in the form of an array.
+
+  echo 'Please choose a domain:<br /><br />';
+
+  foreach ($fileScan AS $domain) { // List each of the stored domains.
+    if (aviewer_isSpecial($domain)) continue; // Don't show ".", "..", etc.
+
+    $domainNoZip = aviewer_stripZip($domain); // Domains can be zipped initially, so remove them if needed.
+
+    echo "<a href=\"{$me}?url={$domainNoZip}/{$homeFile}\">{$domainNoZip}</a><br />";
+  }
+}
+
+else { // URL specified
+  $urlDomain = preg_replace('/^((http|https|ftp|mailto):(\/\/|)|)(([a-zA-Z0-9\.\-\_]+?)\.(com|net|org|info|us|co\.jp))\/(.+)$/', '\\4', $url); // This is prolly the worst way to do this; TODO
+  $urlFile = preg_replace('/^((http|https|ftp|mailto):(\/\/|)|)(([a-zA-Z0-9\.\-\_]+?)\.(com|net|org|info|us|co\.jp))\/(.+)$/', '\\7', $url); // This is prolly the worst way to do this; TODO
+  $urlDirectory = aviewer_dirPart($urlFile);
+  $absPath = $cacheStore . $urlDomain . '/' . $urlFile;
+
+  // Get proper configuration.
+  if (isset($domainConfiguration[$urlDomain])) {
+    $config = $domainConfiguration[$urlDomain];
+  }
+  else {
+    $config = $domainConfiguration['default'];
+  }
+
+  if (!aviewer_inCache($urlDomain)) {
+    $storeScan = scandir($store); // Scan the directory that stores offline domains.
+
+    if (in_array($urlDomain, $storeScan)) { // Check to see if the domain is in the store.
+      symlink("{$store}/{$urlDomain}", "{$cacheStore}/{$urlDomain}");
+    }
+    elseif (in_array($urlDomain . '.zip', $storeScan)) {
+
+    }
+    else { // The domain isn't in the store.
+      if ($config['passthru']) {
+        header('Location: ' . $url);
+      }
+      else {
+        if (!$_SERVER['HTTP_REFERER']) {
+          $data = 'Domain not found: "' . $urlDomain . '"';
+          aviewer_basicTemplate($data);
+        }
+
+        die();
+      }
+    }
+
+    /* TODO: Uncompress */
+  }
+
+  if (is_dir($absPath)) { // Allow (minimal) directory viewing.
+    if (is_file("{$absPath}/{$homeFile}")) { // Automatically redirect to the home/index file if it exists in the directory.
+      header("Location: {$me}?url={$url}/{$homeFile}");
+    }
+    else {
+      $dirFiles = scandir($absPath); // Get all files.
+
+      $data = "<h1>Directory \"{$url}\"</h1><hr />";
+
+      foreach ($dirFiles AS $file) { // List each one.
+        if (aviewer_isSpecial($file)) continue; // Don't show ".", "..", etc.
+
+        $data .= "<a href=\"{$me}?url={$url}/{$file}\">$file</a><br />";
+      }
+
+      aviewer_basicTemplate($data);
+    }
+  }
+  else {
+    if (file_exists($absPath)) {
+      $contents = file_get_contents($absPath); // Get the file contents.
+
+      $urlFileParts = explode('.', $urlFile);
+      $urlFileExt = $urlFileParts[count($urlFileParts) - 1];
+
+      if (!$fileType) {
+        switch ($urlFileExt) { // Attempt to detect file type by extension.
+          case 'html': case 'htm': case 'shtml': case 'php': $fileType = 'html'; break;
+          case 'css': $fileType = 'css'; break;
+          case 'js': $fileType = 'js'; break;
+          default: $fileType = 'other'; break;
+        }
+
+        if ($fileType == 'other') { // Try to autodetect file type by contents.
+          if (preg_match('/^([\ \n]*)(\<\!DOCTYPE|\<html)/i', $contents)) { $fileType = 'html'; }
+        }
+      }
+
+      switch ($fileType) {
+        case 'html':
+        header('Content-type: text/html');
+        echo aviewer_processHtml($contents);
+        break;
+
+        case 'css':
+        header('Content-type: text/css');
+        echo aviewer_processCSS($contents);
+        break;
+
+        case 'js':
+        header('Content-type: text/javascript');
+        echo aviewer_processJavascript($contents);
+        break;
+
+        case 'other':
+        $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+        $mimeType = finfo_file($finfo, $absPath);
+        finfo_close($finfo);
+
+        header('Content-type: ' . $mimeType);
+        if (in_array($urlFileExt, array('zip', 'tar', 'gz', 'bz2', '7z', 'lzma'))) { header('Content-Disposition: *; filename="' . filePart($urlFile) . '"'); }
+
+        echo $contents;
+        break;
+      }
+    }
+    else {
+      if (!$_SERVER['HTTP_REFERER']) {
+        $data = 'File not found: "' . $absPath . '"';
+        //aviewer_basicTemplate($data);
+      }
+
+      die();
+    }
+  }
+}
+?>
---
+++ aviewerFunctions.php
@@ -0,0 +1,300 @@
+<?php
+/*
+   Copyright 2011 Joseph T. Parsons
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+function aviewer_isZip($file) {
+  $file = (string) $file; // God, I wish this could be done in the function line.
+
+  if (preg_match('/\.zip$/', $file)) { // This is prolly the worst way of doing this (like, duh); TODO
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function aviewer_stripZip($file) {
+  $file = (string) $file; // God, I wish this could be done in the function line.
+
+  if (aviewer_isZip($file)) { // This is prolly the worst way of doing this (like, duh); TODO
+    return preg_replace('/^(.+)\.zip$/', '\\1', $file);
+  }
+  else {
+    return $file;
+  }
+}
+
+function aviewer_inCache($domain) {
+  global $cacheStore;
+
+  $fileScan = (array) scandir($cacheStore); // Read the directory and return each file in the form of an array.
+
+  if (in_array($domain, $fileScan)) { // TODO?
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function aviewer_unArchive($text, $maxDepth = 1) { // Unarchive ZIP files, and optionally recursive into the directories and unzip all zipped files in them. Note that zip files follow these rules: (1) if they contain a singular parent directory, the directory will be igored in the created tree; (2) the directory will be named based on the zip name (e.g. fun.zip = /fun); (3) if a directory exists to match the zip name directory, the file will not be unzipped; (4) the zip name directory must NOT contain non-alphanumeric characters; (5) the original zip will not be deleted; it can still be safely referrenced by files if needed (a zip file is assumed to be downloaded when referrenced in $_GET[url]]
+
+}
+
+function aviewer_format($file) { // Attempts to format URLs -- absolute or relative -- so that they can be loaded with the viewer.
+  global $me, $urlDomain, $urlDirectory; // Oh, sue me. I'll make it a class or something later.
+
+  $urlDirectoryLocal = $urlDirectory;
+
+  if (preg_match('/^(http|https|ftp|mailto)\:/i', $file)) { // Domain Included; TODO Optimise
+
+  }
+  elseif (preg_match('/^\//', $file) || !$urlDirectory) { // Absolute Path
+    $file = "{$urlDomain}/{$file}";
+  }
+  else { // Relative Path
+    while (preg_match('/^\.\//', $file)) {
+      $file = preg_replace('/^\.\/(.*)/', '$1', $file);
+    }
+    while (preg_match('/^\.\.\//', $file)) {
+      $file = preg_replace('/^\.\.\/(.*)/', '$1', $file);
+      $urlDirectoryLocal = aviewer_dirPart($urlDirectoryLocal);
+    }
+
+    $file = "{$urlDomain}/{$urlDirectoryLocal}/{$file}";
+  }
+
+  return "{$me}?url={$file}";
+}
+
+function aviewer_dirPart($file) { // Obtain the parent directory of a file or directory by analysing its string value. This will not operate on the directory or file itself.
+  $fileParts = explode('/', $file);
+
+  foreach ($fileParts AS $id => $part) { // Remove all empty elements.
+    if (!$part) {
+      unset($fileParts[$id]);
+    }
+  }
+
+  array_pop($fileParts); // Note: Because of the previous foreach loop, the array index may be corrupted (e.g. the array will be {0 = ele, 2 = ele}), thus making array_pop the only possible means of removing the last element of the array (as opposed to the count method that may be faster).
+
+  implode('/', $fileParts);
+}
+
+function aviewer_filePart($file) { // Obtain the file or directory without its parent directory by analysing its string value. This will not operate on the directory or file itself.
+  $fileParts = explode('/', $file);
+
+  foreach ($fileParts AS $id => $part) { // Remove all empty elements.
+    if (!$part) {
+      unset($fileParts[$id]);
+    }
+  }
+
+  return array_pop($fileParts); // Note: Because of the previous foreach loop, the array index may be corrupted (e.g. the array will be {0 = ele, 2 = ele}), thus making array_pop the only possible means of removing the last element of the array (as opposed to the count method that may be faster).
+}
+
+function aviewer_isSpecial($file) {
+  if ($file === '.' || $file === '..' || $file === '~') { // Yes, the last one isn't normally used; I have my pointless reasons.
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function aviewer_basicTemplate($data, $title = '') {
+  echo "<html>
+  <head>
+    <title>{$title}</title>
+    <style>
+    body {
+      font-family: Ubuntu, sans;
+    }
+    h1 {
+      margin: 0px;
+      padding: 0px;
+    }
+    </style>
+  </head>
+
+  <body>
+    {$data}
+  </body>
+</html>";
+}
+
+// Replaces "<" and ">" (using entitiesHackInner) if within a string.
+function entitiesHackOuter($scriptContent) {
+  $scriptContent = preg_replace("/\"(.+)\"/e", '"\"" . entitiesHackInner("$1") . "\""', $scriptContent);
+  $scriptContent = preg_replace("/'(.+)'/e", '"\'" . entitiesHackInner("$1") . "\'"', $scriptContent);
+
+
+  return $scriptContent;
+}
+
+// Replaces "<" and ">".
+function entitiesHackInner($stringContent) {
+  $stringContent = str_replace('<', '&lt;', $stringContent);
+  $stringContent = str_replace('>', '&gt;', $stringContent);
+
+  return $stringContent;
+}
+
+function aviewer_processHtml($contents) {
+  global $config; // Yes, I will make this a class so this is less annoying.
+
+  if ($config['removeExtra']) {
+    $contents = preg_replace('/\<\?xml(.+)\?\>/', '', $contents);
+    $contents = preg_replace('/\<\!--(.*?)--\>/ism', '', $contents); // Get rid of comments (cleans up the DOM at times, making things faster). We do not remove commnets if they are a part of JavaScript.
+  }
+
+  if ($config['badEntitiesHack']) {
+//    preg_match_all('/\<script(.*?)\>(.*?)<\/script\>/s',$contents,$return);
+//    print_r($return);
+    $contents = preg_replace('/\<script(.*?)\>(.*?)\<\/script\>/es','"<script$1>" . entitiesHackOuter("$2") . "</script>"',$contents);
+  }
+
+  libxml_use_internal_errors(true); // Stop the loadHtml call from spitting out a million errors.
+  $doc = new DOMDocument(); // Initiate the PHP DomDocument.
+  $doc->preserveWhiteSpace = false; // Don't worry about annoying whitespace.
+  $doc->loadHTML($contents); // Load the HTML.
+
+  // Process LINK tags
+  $linkList = $doc->getElementsByTagName('link');
+  for ($i = 0; $i < $linkList->length; $i++) {
+    if ($linkList->item($i)->hasAttribute('href')) {
+      if ($linkList->item($i)->getAttribute('type') == 'text/css' || $linkList->item($i)->getAttribute('rel') == 'stylesheet') {
+        $linkList->item($i)->setAttribute('href', aviewer_format($linkList->item($i)->getAttribute('href') . '&type=css'));
+      }
+      else {
+        $linkList->item($i)->setAttribute('href', aviewer_format($linkList->item($i)->getAttribute('href')));
+      }
+    }
+  }
+
+  // Process SCRIPT tags.
+  $scriptList = $doc->getElementsByTagName('script');
+  $scriptDrop = array();
+  for ($i = 0; $i < $scriptList->length; $i++) {
+    if ($scriptList->item($i)->hasAttribute('src')) {
+      $scriptList->item($i)->setAttribute('src', aviewer_format($scriptList->item($i)->getAttribute('src')) . '&type=js');
+    }
+    else {
+      if ($config['scriptDispose']) {
+        $scriptDrop[] = $scriptList->item($i);
+      }
+      else {
+        $scriptList->item($i)->nodeValue = aviewer_processJavascript($scriptList->item($i)->nodeValue);
+      }
+    }
+  }
+  foreach ($scriptDrop AS $drop) {
+    $drop->parentNode->removeChild($drop);
+  }
+
+  // Process STYLE tags.
+  $styleList = $doc->getElementsByTagName('style');
+  for ($i = 0; $i < $styleList->length; $i++) {
+    $styleList->item($i)->nodeValue = aviewer_processCSS($styleList->item($i)->nodeValue);
+  }
+
+  // Process BASE tags.
+  $baseList = $doc->getElementsByTagName('base');
+  for ($i = 0; $i < $scriptList->length; $i++) {
+    // TODO: Change Base (e.g. $urlDirectory)
+  }
+
+  // Process IMG, VIDEO, AUDIO, IFRAME tags
+  foreach (array('img', 'video', 'audio', 'iframe', 'applet') AS $ele) {
+    $imgList = $doc->getElementsByTagName($ele);
+    for ($i = 0; $i < $imgList->length; $i++) {
+      if ($imgList->item($i)->hasAttribute('src')) {
+        $imgList->item($i)->setAttribute('src', aviewer_format($imgList->item($i)->getAttribute('src')) . '&type=other');
+      }
+    }
+  }
+
+  // Process A, AREA (image map) tags
+  foreach (array('a', 'area') AS $ele) {
+    $aList = $doc->getElementsByTagName($ele);
+    for ($i = 0; $i < $aList->length; $i++) {
+      if ($aList->item($i)->hasAttribute('href')) {
+        $aList->item($i)->setAttribute('href', aviewer_format($aList->item($i)->getAttribute('href')));
+      }
+    }
+  }
+
+  // Process BODY, TABLE, TD, and TH tags w/ backgrounds. TABLE, TD & TH do support the background tag, but it was an extension of both Netscape and IE way back, and today most browsers still recognise it and will add a background image as appropriate, so... we have to support it.
+  foreach (array('body', 'table', 'td', 'th') AS $ele) {
+    $aList = $doc->getElementsByTagName($ele);
+    for ($i = 0; $i < $aList->length; $i++) {
+      if ($aList->item($i)->hasAttribute('background')) {
+        $aList->item($i)->setAttribute('background', aviewer_format($aList->item($i)->getAttribute('background')));
+      }
+    }
+  }
+
+  // Process Option Links; some sites will store links in OPTION tags and then sue Javascript to link to them. Thus, if the hack is enabled, we will try to cope.
+  if ($config['selectHack']) {
+    $optionList = $doc->getElementsByTagName('option');
+    for ($i = 0; $i < $optionList->length; $i++) {
+      if ($optionList->item($i)->hasAttribute('value')) {
+        $optionValue = $optionList->item($i)->getAttribute('value');
+        if (preg_match('/\.(htm|html|php|shtml|\/)$/', $optionValue)) { // TODO Optimise
+          $optionList->item($i)->setAttribute('value', aviewer_format($optionValue));
+        }
+      }
+    }
+  }
+
+  // This is the meta-refresh hack, which tries to fix meta-refresh headers that may in some cases automatically redirect a page, similar to <a href>. This is hard to work with, and in general sites wishing to achieve this will often implement it instead using headers (which, due to the nature of an archive, will not be transmitted and thus we don't have to worry about modifying them) or using JavaScript (which is never easy to implement, though in some cases it still works). An example: <meta http-equiv="Refresh" content="5; URL=http://www.google.com/index">
+  if ($config['metaHack']) {
+    $metaList = $doc->getElementsByTagName('meta');
+    for ($i = 0; $i < $metaList->length; $i++) {
+      if ($metaList->item($i)->hasAttribute('http-equiv') && $metaList->item($i)->hasAttribute('content')) {
+        if (strtolower($metaList->item($i)->getAttribute('http-equiv')) == 'refresh') {
+          $metaList->item($i)->setAttribute('content', preg_replace('/^(.*)url=([^ ]+)(.*)$/ies', '"$1" . aviewer_format("$2") . "$3"', $metaList->item($i)->getAttribute('content')));
+        }
+      }
+    }
+  }
+
+  return $doc->saveHTML(); // Return the updated data.
+}
+
+function aviewer_processJavascript($contents) {
+  global $config;
+
+  $contents = preg_replace('/\/\*(.*?)\*\//is', '', $contents); // Removes comments.
+
+  if ($config['scriptEccentric']) { // Convert anything that appears to be a suspect file. Because of the nature of this, there is a high chance stuff will break if $scriptEccentric is enabled. But, it allows some sites to work properly that otherwise wouldn't.
+    $contents = preg_replace('/(([a-zA-Z0-9\_\-\/]+)\.(php|htm|html|css|js)[^a-zA-Z0-9])/ie', 'aviewer_format("$1")', $contents); // Note that if the extension is followed by a letter or integer, it is possibly a part of a JavaScript property, which we don't want to convert.
+  }
+  else { // Convert strings that contain files ending with suspect extensions.
+    $contents = preg_replace('/("|\')(([a-zA-Z0-9\_\-\/]+)\.(php|htm|html|css|js))\1/ie', 'stripslashes("$1") . aviewer_format("$2") . stripslashes("$1")', $contents);
+  }
+
+  return $contents; // Return the updated data.
+}
+
+function aviewer_processCSS($contents) {
+  $contents = preg_replace('/\/\*(.*?)\*\//is', '', $contents); // Removes comments.
+  $contents = str_replace(';',";\n", $contents); // Fixes an annoying REGEX quirk below; I won't go into it.
+  $contents = preg_replace('/url\((\'|"|)(.+)\\1\)/ei', '\'url($1\' . aviewer_format("$2") . \'$1)\'', $contents); // CSS images are handled with this.
+
+  return $contents; // Return the updated data.
+}
+?>
